2eb614e8bf12ca4bfd07a44a6a36064508ed3a98
Temilola <temz4rill@gmail.com>
2021-06-06 23:15:48 -0400

    Implement Trivia app

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -14,6 +15,9 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
+        binding.tryAgainButton.setOnClickListener { view ->
+            view.findNavController().navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
+        }
         return binding.root
     }
 }
